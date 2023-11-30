4cf6a60b900fb4ad68982a30debc1f4dbec1a753
Akhil Reddy Depa <143060945+Akhil2402@users.noreply.github.com>
2023-11-29 19:31:17 -0600

    Commit all

--- app.js
+++ app.js
@@ -34,8 +34,16 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function(){ console.log("Connection to DB succeeded")});
 
+ var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var snakeRouter = require('./routes/snake');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+var resourceRouter = require('./routes/resource');
 var snake = require('./models/snake');
 
+
+// view engine setup
 var app = express();
 async function recreateDB(){
   // Delete everything
@@ -64,15 +72,6 @@ async function recreateDB(){
  }
  let reseed = true;
  if (reseed) { recreateDB();}
-
- var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var snakeRouter = require('./routes/snake');
-var boardRouter = require('./routes/board');
-var chooseRouter = require('./routes/choose');
-var resourceRouter = require('./routes/resource');
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -96,7 +95,10 @@ app.use('/snake', snakeRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
-
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/snake.js
+++ models/snake.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const snakeSchema = mongoose.Schema({
 snake_color: String,
-snake_breed: String,
-snake_price: Number
+snake_breed: {type:String,length:15},
+snake_price: {type:Number,min:1000,max:200000}
 })
 module.exports = mongoose.model("snake",snakeSchema)
